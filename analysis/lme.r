9dc77ec3cec42eef1fc755c99619544f79d24a73
Phillip Alday <phillip.alday@staff.uni-marburg.de>
2014-05-26 17:19:14 +0200

    clean up

--- analysis/lme.r
+++ analysis/lme.r
@@ -1,4 +1,7 @@
 library(lme4)
+library(effects)
+
+setwd(normalizePath("."))
 
 # (männl: 0)
 
@@ -7,48 +10,16 @@ library(lme4)
 # note: real gender used, so sex alone, no interaction term, should be optimal
 
 # out_all = ['subj,word,assoc_i,sex_port,sex_ger,sex,language,rating']
-sourcefile<-paste("../preproc/boroditsky_all.csv",sep=",")
-dat<-read.table(sourcefile,sep=",",header=TRUE)
-
-dat$subj <- as.factor(dat$subj)
-dat$language <- as.factor(dat$language)
-dat$word <- as.factor(dat$word)
-dat$sex_port <- as.factor(dat$sex_port)
-dat$sex_ger <- as.factor(dat$sex_ger)
-dat$sex <- as.factor(dat$sex)
-dat$assoc_i <- as.factor(dat$assoc_i)
-dat$rater_i <- as.factor(dat$rater_i)
+dat <- read.csv(normalizePath("../preproc/boroditsky_all.csv")
+                ,header=TRUE
+                ,colClasses="factor")
 
 
-
-modl1 <- glmer(rating ~ sex_ger:language + (1|rater_i) + (1|assoc_i/word/language) + (1|assoc_i/subj/language) , data = dat, family=binomial(link = 'logit'))
-
-
-modl1 <- glmer(rating ~ sex + language + (1|rater_i) + (1|assoc_i/word/language) +  (1|subj) , data = dat, family=binomial(link = 'logit'),verbose=TRUE)
+basic <- glmer(rating ~ sex * language + (1|rater_i) + (1|word/language) +  (1|subj), data = dat, family=binomial)
+basic.probit <- glmer(rating ~ sex * language + (1|rater_i) + (1|word/language) +  (1|subj), data = dat, family=binomial(link="probit"))
 
 
-
-modl2 <- lmer(rating ~ language + (1|rater_i)  + (1|assoc_i/word/language) + (1|assoc_i/subj/language) , data = dat, family=binomial)
+plot(allEffects(germansex <- glmer(rating ~ sex_ger * language + (1|rater_i) + (1|word/language) +  (1|subj), data = dat, family=binomial)))
 
 summary(modl1)
-summary(modl2)
 
-anova(modl1, modl2, test = "F")
-
-# xyplot(rating ~ sex|language + subj + word, data = dat)
-
-
-modl1 <- lmer(rating ~ sex_ger:language + (1|rater_i) + (1|word/language) + (1|subj/language) , data = dat)
-
-
-
-
-modl3 <- lmer(rating ~ sex + language + (1|rater_i) + (1|word) + (1|subj) , data = dat)
-
-modl4 <- lmer(rating ~ language +  (1|word) + (1|subj) , data = dat)
-
-summary(modl3)
-summary(modl4)
-
-anova(modl1, modl3, test = "F")
-anova(modl2, modl4, test = "F")
